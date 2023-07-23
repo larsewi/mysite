bfff8987d3e5a5b963ee89c0edbd92617bf2c1a2
Lars Erik Wik <lars.erik.wik@northern.tech>
2023-07-23 18:34:34 +0200

    Add signup page
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path("polls/", include("polls.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
     path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
